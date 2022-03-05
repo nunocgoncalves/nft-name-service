8c9121d3697031a52c171b91936cc9fc38a3cd21
nunomigueldev <nunomcgoncalves2005@gmail.com>
2022-03-05 17:24:44 +0000

    Added function to fetch all the domains, to check domain validity and enabled costum errors. Reviewed code and comments.

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -18,15 +18,23 @@ contract Domains is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
+  // Owner and TLD constants
   address payable public owner;
   string public tld;
 	
-  // NFT as SVG
+  // Create NFT SVG
   string svgPartOne = '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#B)" d="M0 0h270v270H0z"/><defs><filter id="A" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-10.081 6.032-6.85 3.934-10.081 6.032c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616c-.384-.665-.594-1.418-.608-2.187v-9.31c-.013-.775.185-1.538.572-2.208a4.25 4.25 0 0 1 1.625-1.595l7.884-4.59c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v6.032l6.85-4.065v-6.032c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595L41.456 24.59c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595c-.387.67-.585 1.434-.572 2.208v17.441c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l10.081-5.901 6.85-4.065 10.081-5.901c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v9.311c.013.775-.185 1.538-.572 2.208a4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616c-.385-.665-.594-1.418-.608-2.187v-6.032l-6.85 4.065v6.032c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l14.864-8.655c.657-.394 1.204-.95 1.589-1.616s.594-1.418.609-2.187V55.538c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/><defs><linearGradient id="B" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#cb5eee"/><stop offset="1" stop-color="#0cd7e4" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#A)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
   string svgPartTwo = '</text></svg>';
 
+  // Mappings
   mapping(string => address) public domains;
   mapping(string => string) public records;
+  mapping (uint => string) public names;
+
+  // Set custom errors
+  error Unauthorized();
+  error AlreadyRegistered();
+  error InvalidName(string name);
 
   constructor(string memory _tld) payable ERC721("Polygon Name Service", "PNS") {
     owner = payable(msg.sender);
@@ -35,13 +43,12 @@ contract Domains is ERC721URIStorage {
   }
 
   function getAddress(string calldata name) public view returns (address) {
-    // Check that the owner is the transaction sender
     return domains[name];
   }
 
   function setRecord(string calldata name, string calldata record) public {
-    // Check that the owner is the transaction sender
-    require(domains[name] == msg.sender);
+    // If the message sender does not own the domain revert transaction (Unauthorized)
+    if (msg.sender != domains[name]) revert Unauthorized();
     records[name] = record;
   }
 
@@ -51,25 +58,36 @@ contract Domains is ERC721URIStorage {
 
   function price(string calldata name) public pure returns(uint) {
     uint len = StringUtils.strlen(name);
-    require(len > 0);
-    if (len == 3) {
+    require(len > 1);
+    if (len == 2) {
         return 5 * 10**14; // 0.005 MATIC = 5 000 000 000 000 000 (18 decimals)
-    } else if (len == 4) {
+    } else if (len <= 4) {
         return 3 * 10**14; // 0.003 MATIC = 5 000 000 000 000 000 (18 decimals)
     } else {
         return 1 * 10**14; // 0.001 MATIC = 5 000 000 000 000 000 (18 decimals)
     }
   }
 
-  function register(string calldata name) public payable {
-    require(domains[name] == address(0));
+  // Check for a valid domain length
+  function valid(string calldata name) public pure returns(bool) {
+  return StringUtils.strlen(name) >= 2 && StringUtils.strlen(name) <= 15;
+  }
 
+  function register(string calldata name) public payable {
+    
+    // If the domain already exists revert transaction
+    if (domains[name] != address(0)) revert AlreadyRegistered();
+    // If the domain name is invalid (too short or too long) revert the transaction
+    if (!valid(name)) revert InvalidName(name);
+    
+    // Check for transaction payment
     uint256 _price = price(name);
     require(msg.value >= _price, "Not enough MATIC paid");
 		
-	// Combine the name passed into the function with the TLD
+	  // Combine the name passed into the function with the TLD
     string memory _name = string(abi.encodePacked(name, ".", tld));
-	// Create the SVG for the NFT
+
+	  // Generate the SVG for the NFT
     string memory finalSvg = string(abi.encodePacked(svgPartOne, _name, svgPartTwo));
     uint256 newRecordId = _tokenIds.current();
   	uint256 length = StringUtils.strlen(name);
@@ -77,14 +95,14 @@ contract Domains is ERC721URIStorage {
 
     console.log("Registering %s.%s on the contract with tokenID %d", name, tld, newRecordId);
 
-	// JSON metadata for the NFT
+	  // JSON metadata for the NFT
     string memory json = Base64.encode(
       bytes(
         string(
           abi.encodePacked(
             '{"name": "',
             _name,
-            '", "description": "A domain on the ethereum name service", "image": "data:image/svg+xml;base64,',
+            '", "description": "A domain on the polygon domain service", "image": "data:image/svg+xml;base64,',
             Base64.encode(bytes(finalSvg)),
             '","length":"',
             strLen,
@@ -93,9 +111,11 @@ contract Domains is ERC721URIStorage {
         )
       )
     );
-
+    
+    // Encode NFT
     string memory finalTokenUri = string( abi.encodePacked("data:application/json;base64,", json));
 
+  // Print encoded Base64 JSON
 	console.log("\n--------------------------------------------------------");
 	console.log("Final tokenURI", finalTokenUri);
 	console.log("--------------------------------------------------------\n");
@@ -104,9 +124,25 @@ contract Domains is ERC721URIStorage {
     _setTokenURI(newRecordId, finalTokenUri);
     
     domains[name] = msg.sender;
+    names[newRecordId] = name;
 
     _tokenIds.increment();
   }
+
+  // Get all minted domains
+  function getAllNames() public view returns (string[] memory) {
+  console.log("Getting all minted domains from contract");
+  string[] memory allNames = new string[](_tokenIds.current());
+  for (uint i = 0; i < _tokenIds.current(); i++) {
+    allNames[i] = names[i];
+    console.log("Domain for token %d is %s", i, allNames[i]);
+  }
+
+  return allNames;
+
+  }
+
+
   modifier onlyOwner() {
   require(isOwner());
   _;
@@ -121,4 +157,5 @@ contract Domains is ERC721URIStorage {
 	  (bool success, ) = msg.sender.call{value: amount}("");
 	  require(success, "Failed to withdraw Matic");
   } 
+
 }
